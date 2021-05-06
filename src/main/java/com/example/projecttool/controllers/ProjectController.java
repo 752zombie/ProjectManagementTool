14fa4ab234d48b47e6f648824b968a594b4ae0ce
Matthias <mdjer@hotmail.com>
2021-05-06 16:31:20 +0200

    added ability to add row to project

--- src/main/java/com/example/projecttool/controllers/ProjectController.java
+++ src/main/java/com/example/projecttool/controllers/ProjectController.java
@@ -1,39 +1,66 @@
 package com.example.projecttool.controllers;
 
+import com.example.projecttool.models.User;
+import com.example.projecttool.models.project.Project;
+import com.example.projecttool.models.project.ProjectTest;
 import com.example.projecttool.repositories.ProjectRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @Controller
 public class ProjectController {
 
     @GetMapping("/create-project")
-    public String profilePage() {
-        return "create-project";
+    public String createProject() {
+
+
+    return "project/create-project";
     }
 
 
-    @PostMapping("edit-project")
+    @PostMapping("/edit-project")
     public String editProject(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("description") String description) {
 
 
         try {
 
+          ProjectRepository.editProject(id, name, description);
 
-
-
-
-            ProjectRepository.editProject(id, name, description);
-
-            return "create-project";
+            return "redirect:/old-project";
 
         } catch (SQLException s) {
-            return "edit-failed";
+            return "project/edit-failed";
         }
     }
+
+    @GetMapping("old-project")
+    public String getProject(Model model, HttpSession session) {
+
+
+        User user = (User) session.getAttribute("user");
+        ArrayList<ProjectTest> projectList = ProjectRepository.getProject();
+
+
+        model.addAttribute("projectList", projectList);
+
+        return "project/old-project";
+
+    }
+
+    @PostMapping("add-row")
+    public String addRowToProject(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("description") String description){
+
+        ProjectRepository.addRowToProject(id, name, description);
+
+        return "redirect:/old-project";
+    }
+
 
 }
