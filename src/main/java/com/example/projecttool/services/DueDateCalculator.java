dbf6f264bc08c08adec00fb343d15586ed8ce6b3
Matthias <mdjer@hotmail.com>
2021-05-13 21:27:54 +0200

    added weekendsOFF method

--- src/main/java/com/example/projecttool/services/DueDateCalculator.java
+++ src/main/java/com/example/projecttool/services/DueDateCalculator.java
@@ -1,33 +1,48 @@
 package com.example.projecttool.services;
 
-import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 
 public class DueDateCalculator {
 
     public String dueDate(int hoursDay, int hoursTotal, String startDate) {
 
-        String dueDate;
-        int daysToFinish;
+        boolean weekends = true;
+        int daysToFinish = (hoursTotal / hoursDay);
 
         // IMPROVED VERSION WITH AMOUNT OF EMPLOYEES //
                //  int totalWorkHoursDay = hoursDay * employees;
                // int daysToFinish = hoursTotal / totalWorkHoursDay;
 
+    if (weekends) {
 
-         daysToFinish = (hoursTotal/hoursDay);
-
-
-        dueDate = dateAdder(daysToFinish, startDate);
+        String dueDate = dateAdderWithWeekends(daysToFinish, startDate);
 
 
         return dueDate;
+    }
+    else {
+        LocalDate str = LocalDate.parse(startDate);
+
+        LocalDate withoutWeekends = addDaysSkippingWeekends(str, daysToFinish);
+
+        String dateWithoutWeekends = withoutWeekends.toString();
+
+
+        return dateWithoutWeekends;
+
 
     }
 
-    public String dateAdder(int daysToFinish, String startDate) {
+    }
+
+    public String dateAdderWithWeekends(int daysToFinish, String startDate) {
 
         //Specifying date format that matches the given date
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -48,6 +63,19 @@ public class DueDateCalculator {
 
         return dueDate;
 
+    }
+
+
+    public static LocalDate addDaysSkippingWeekends(LocalDate date, int days) {
+        LocalDate result = date;
+        int addedDays = 0;
+        while (addedDays < days) {
+            result = result.plusDays(1);
+            if (!(result.getDayOfWeek() == DayOfWeek.SATURDAY || result.getDayOfWeek() == DayOfWeek.SUNDAY)) {
+                ++addedDays;
+            }
+        }
+        return result;
     }
 
 
